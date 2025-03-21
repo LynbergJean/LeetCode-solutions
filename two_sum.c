ce54b76d39543bbfa2db67411b4387b6897b960a
Lynberg Jean <138824964+LynbergJean@users.noreply.github.com>
2025-03-21 10:20:37 -0400

    Update two_sum.c

--- two_sum.c
+++ two_sum.c
@@ -4,7 +4,10 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    //allocate space for the array
     int *arr = (int*) malloc(sizeof(int) * *returnSize);
 
+    //loop through the array
+    for
     
 }
