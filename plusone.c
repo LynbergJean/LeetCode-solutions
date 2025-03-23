f5d9f039ec30682c75cf83b9f81eb900a223653a
Lynberg Jean <138824964+LynbergJean@users.noreply.github.com>
2025-03-22 23:35:01 -0400

    Create plusone.c

---
+++ plusone.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int *plusone(int *digits, int digitsSize, int *returnSize);
+
+int main(){
+  return 0;
+}
+
+int *plusone(int *digits, int digitsSize, int *returnSize)
+{
+  
+}
