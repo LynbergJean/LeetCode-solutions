8e632b93f4774375ec92b77c4e39aaa4e901ff67
Lynberg Jean <138824964+LynbergJean@users.noreply.github.com>
2025-03-23 23:52:10 -0400

    Create contains_duplicate.c

---
+++ contains_duplicate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+bool containsNearbyDuplicate(int* nums, int numsSize, int k);
+
+int main()
+{
+  return 0;
+}
+
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+
+
+}
