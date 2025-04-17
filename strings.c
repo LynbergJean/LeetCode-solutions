c8567ee7bd3bbb072e2f4e80b1f6d77c0edda4ef
Lynberg Jean <138824964+LynbergJean@users.noreply.github.com>
2025-04-16 23:48:41 -0400

    Create strings.c

---
+++ strings.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+//given a string create a new word using the first letter of each word in the string
+void read_line
+int main(){
+  int n;
+  scanf("%d",&n);
+
+  char arr1[100];
+  
+  int i = 0;
+  while(i < n){
+    
+  }
+}
