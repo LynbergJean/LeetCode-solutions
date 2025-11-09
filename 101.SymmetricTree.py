3daaf9fcfca123eb0289b95d1000c6553e8ef045
lynbergjean <lynberg281@gmail.com>
2025-11-09 01:07:05 -0500

    for symmetry check left_left and right_right

---
+++ 101.SymmetricTree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+
+        queue = deque([root])
+        temp = queue.popleft()
+        #add the left and right node
+
+        queue.append(temp.left)
+        queue.append(temp.right)
+
+        while queue:
+            t1 = queue.popleft()
+            t2 = queue.popleft()
+
+            if t1 == None and t2 == None:
+                continue
+
+            if t1 == None or t2 == None:
+                return False
+
+            if t1.val != t2.val:
+                return False
+
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+        
+        return True
