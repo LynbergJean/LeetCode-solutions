dbc1a064e9783cad89e576b41ce444505fdbfc15
lynbergjean <lynberg281@gmail.com>
2025-11-27 10:45:00 -0500

    stacks check if theyre empty first

---
+++ 20.validParenthesis.cpp
@@ -0,0 +1,31 @@
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char,char> m = {
+            {')','('},
+            {'}','{'},
+            {']','['}
+        };
+
+        stack<char> st;
+
+        for(char c: s)
+        {
+            if(c == '(' || c == '{' || c == '[')
+            {
+                st.push(c);
+
+            }else{
+                if( st.empty() || st.top() != m[c] ) return false;
+                st.pop();
+            }
+            
+        }
+
+        return st.empty();
+
+    }
+};
