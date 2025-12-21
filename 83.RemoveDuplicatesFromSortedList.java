58f3aefc5b756fb466fd114c1973f8033646027d
lynbergjean <lynberg281@gmail.com>
2025-12-21 15:36:41 -0500

    removed junk

--- 83.RemoveDuplicatesFromSortedList.java
+++ 83.RemoveDuplicatesFromSortedList.java
@@ -1,12 +1,5 @@
-import java.util.*;
 
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
+
 
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
